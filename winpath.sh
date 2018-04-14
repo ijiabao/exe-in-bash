43635f890df2fcaa551b18a4bec51a8eea06d193
Jiabao <ijiabao@qq.com>
2018-04-14 21:30:54 +0800

    忽略空参数和减号开头的
    # fix 忽略空参数，或参数以"-"开头

--- winpath.sh
+++ winpath.sh
@@ -4,12 +4,12 @@
 # fixed symbolic link
 # by ijiabao
 
-[[ ${1:0:1} == "-" ]] && echo $1 && exit 0
+[[ ${1:0:1}=="-" || $1=="" ]] && echo $1 && exit 0
 
 readlink -f `cd $(dirname $1); pwd`/`basename $1` | sed -e 's@^/mnt/\(\w\)/@\1:/@' -e 's@/@\\@g'
 
 exit 0
-# fix 参数不以"-"开头
+# fix 忽略空参数，或参数以"-"开头
 # 拆解: path,file为参数1, full为完整路径, real为真实路径, 最后做个正则替换
 path=`dirname "$1"`
 file=`basename "$1"`
