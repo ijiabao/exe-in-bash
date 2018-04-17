2a83605c0d881193189c6de0432c1f89004df693
Jiabao <ijiabao@qq.com>
2018-04-17 21:18:18 +0800

    Update winpath.sh

--- winpath.sh
+++ winpath.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 # filename: {/usr,/usr/local}/bin/winpath
+# 将参数1转换为windows路径 (子系统用)
 # transform arg1 to full windows path
 # fixed symbolic link
 # by ijiabao
 
-[[ ${1:0:1}=="-" || $1=="" ]] && echo $1 && exit 0
+# fix: ignore empth, or start with "-"
+[[ ${1:0:1} = "-" || $1 = "" ]] && echo "nook" && exit 0
 
 readlink -f `cd $(dirname $1); pwd`/`basename $1` | sed -e 's@^/mnt/\(\w\)/@\1:/@' -e 's@/@\\@g'
-
-exit 0
-# fix 忽略空参数，或参数以"-"开头
-# 拆解: path,file为参数1, full为完整路径, real为真实路径, 最后做个正则替换
-path=`dirname "$1"`
-file=`basename "$1"`
-full=`cd $path; pwd`/$file
-real=`readlink -f $full`
-# 盘符和斜线替换：`/mnt/d` => `d:`, `/` => `\`
-echo $real | sed -e 's@^/mnt/\(\w\)/@\1:/@' -e 's@/@\\@g'
