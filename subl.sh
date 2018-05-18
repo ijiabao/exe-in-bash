a1b154a5224b728bf4bac7e24a5a74f3644e2e4a
Jiabao <ijiabao@qq.com>
2018-05-18 09:47:16 +0800

    Delete subl.sh

--- subl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# filename: subl.sh, copy and named to /usr/bin/subl
-# by: ijiabao, fixed open file like : /mnt/d/foo/bar/file.ext
-# run subl.exe in bash ^_^
-# exe文件路径需手动修改
-/mnt/c/Program\ Files/Sublime\ Text\ Build\ 3143\ x64/subl.exe `winpath $1`
